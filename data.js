5393d1df35a6d3586084d3026d7fd19ada2e350c
Eliot <mr.ear84@gmail.com>
2015-12-03 09:54:01 -0500

    adding further detail

---
+++ data.js
@@ -0,0 +1,81 @@
+
+
+
+/*
+
+HISTORIAL VS PRESENT COMPARISONS
+
+
+
+    WHITE VS ETHNIC RACIAL COMPARISON
+        - How do ethnicities compare on ability to speak multiple languages
+
+
+Native Born Citizen 5 years or older only spoke English at Home percentage: 
+
+Total: 279,012,712
+
+http://factfinder.census.gov/faces/tableservices/jsf/pages/productview.xhtml?src=bkmk
+
+People a
+2014: 97.2
+2013: 97.2
+2012: 97.3
+2011: 97.3
+2010: 97.4
+2009: 97.4
+2008: 97.4
+2007: 97.4
+2006: 97.4
+
+
+
+
+
+Naturalized Citizen:
+
+
+
+
+1890 Censes: People 10 years old and over who did not speak english were 
+             asked what language they spoke.
+
+1900 Censes: People 10 years old and over who did not speak english were 
+             asked what language they spoke.
+
+1910 Census: Foreign Born people were asked abou their "mother-tongue" -
+			 language. Spoken in the household while growing up.
+
+1920 Census: Foreign Born people were asked abou their "mother-tongue" -
+			 language. Spoken in the household while growing up.
+
+1930 Census: Foreign Born people were asked abou their "mother-tongue" -
+			 language. Spoken in the household while growing up.
+
+1960 Census: Foreign Born people were asked abou their "mother-tongue" -
+			 language. Spoken in the household while growing up.
+
+1980 - 2009 Census: 
+			 1. Does this person speak a language other than English at home?
+			 2. What is the language
+			 3. How well does this person speak English?
+
+2010 - Present Annual American Community Survey:
+			 1. Does this person speak a language other than English at home?
+			 2. What is the language
+			 3. How well does this person speak English?
+
+
+
+
+	Ethnic vs racial comparison
+
+	whites dont learn a second language but other ethnic groups may.
+
+	Historical Data... longitude data
+	Temporal maps
+
+
+	past to present comparison
+
+	*/
