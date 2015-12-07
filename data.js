68413ba0acbe5189decc2a2866596aab0e36f3fc
eliot84 <mr.ear84@gmail.com>
2015-12-07 17:14:16 -0500

    adding updated chart work

--- data.js
+++ data.js
@@ -79,3 +79,31 @@ Naturalized Citizen:
 	past to present comparison
 
 	*/
+
+
+/*
+Top Languages Other than English Spoken in 1980 and Changes in Relative Rank, 1990-2010
+
+http://www.census.gov/dataviz/visualizations/045/508.php
+(example) http://www.census.gov/dataviz/visualizations/045/ (Example)
+	                                           	1980               	1990	         2000	         2010
+Spoke a language other than English at home (1)	23,060,040	        31,844,979	     46,951,595	     59,542,596
+Spanish or Spanish Creole                   (2)	11,116,194	        17,345,064	     28,101,052	     36,995,602
+Chinese	                                           630,806	         1,319,462	      2,022,143	      2,808,692
+French                                      (2)	 1,550,751	         1,930,404	      2,097,206       2,069,352
+Tagalog	                                           474,150	           843,251	      1,224,241	      1,573,720
+Vietnamese	                                       197,588	           507,069	      1,009,627	      1,381,488
+Korean	                                           266,280	           626,478         	894,063	      1,137,325
+German	                                         1,586,593	         1,547,987	      1,383,442	      1,067,651
+Russian	                                           173,226	           241,798         	706,242     	854,955
+Italian	                                         1,618,344         	 1,308,648	      1,008,370	        725,223
+Portuguese                                   (2)   351,875	           430,610	        564,630      	688,326
+Polish	                                           820,647	           723,483	        667,414	        608,333
+Japanese	                                       336,318	           427,657	        477,997	        443,497
+Persian	                                           106,992	           201,865	        312,085	        381,408
+Greek	                                           401,443         	   388,260	        365,436	        307,178
+Serbo-Croatian	                                   150,255	           142,078         	233,865	        284,077
+Armenian	                                       100,634	           149,694	        202,708      	240,402
+Yiddish	                                           315,953	           213,064	        178,945	        154,763
+
+*/
